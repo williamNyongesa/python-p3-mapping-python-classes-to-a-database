7412c7561122f4e56bb2aa68fedae219747bb7b8
William Nyongesa <marakawilliam@gmail.com>
2023-09-28 06:41:49 +0300

    completed assignment

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,34 @@
-from config import CONN, CURSOR
+import sqlite3
+
+CONNECT = sqlite3.connect('music.db')
+CURSOR = CONNECT.cursor()
 
 class Song:
-    pass
+    def __init__(self, name, album, id = None):
+        self.id = id
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = """CREATE TABLE 
+        IF NOT EXISTS songs (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            album TEXT
+        )"""
+        CURSOR.execute(sql)
+        CONNECT.commit()
+
+    def save(self):
+        sql = """INSERT INTO songs (name, album) VALUES (?, ?)"""
+        CURSOR.execute(sql, (self.name, self.album))
+        CONNECT.commit()
+
+    @classmethod
+    def create(cls, name, album):
+        song = cls(name, album)  
+        song.save() 
+        song.id = CURSOR.lastrowid
+        return song  
+
